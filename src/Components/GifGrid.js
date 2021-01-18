b4c2d7d286ab743169240e8386bccc50439644d8
vinjadevix <vinjadevix@gmail.com>
2021-01-18 18:13:07 +0100

    gif search done

--- src/Components/GifGrid.js
+++ src/Components/GifGrid.js
@@ -1,34 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useFetchGifs } from "../Hooks/useFetchGifs";
 import { GifGridItem } from "./GifGridItem";
-export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    getGifs();
-  }, []);
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
-      category
-    )}&limit=10&api_key=jCK15XVVzdCulTUz8SyZse0cgdEtWYgM`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
 
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    console.log("====================================");
-    console.log(gifs);
-    console.log("====================================");
-    setImages(gifs);
-  };
+export const GifGrid = ({ category }) => {
+  const { data, loading } = useFetchGifs(category);
+
   return (
     <>
       <h2> {category}</h2>
+
+      {loading && <p>Cargando...</p>}
+
       <section className="GifGrid">
-        {images.map((img) => (
+        {data.map((img) => (
           <GifGridItem key={img.id} {...img}></GifGridItem>
         ))}
       </section>
